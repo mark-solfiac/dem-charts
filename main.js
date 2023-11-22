e1d1e3e39605a84ca0a20a6b10b98bf6a3283d48
Mark Solfiac <mark.solfiac.100@gmail.com>
2023-11-22 00:08:05 +0000

    Graph with data WIP

--- main.js
+++ main.js
@@ -1,39 +1,122 @@
-const ctx = document.getElementById('populationPyramid').getContext('2d');
+const ctx1 = document.getElementById("pop-pyramid").getContext("2d");
+const ctx2 = document.getElementById("pop-chart").getContext("2d");
+const ctx3 = document.getElementById("pop-percentages").getContext("2d");
 
-const data = {
-  labels: ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80+'],
-  datasets: [{
-    label: 'Male',
-    data: [-10, -20, -30, -40, -50, -60, -70, -80, -90, -100, -110, -120, -130, -140, -150, -160, -170], // negative values for left side
-    backgroundColor: 'blue'
-  }, {
-    label: 'Female',
-    data: [15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125, 135, 145, 155, 165, 175], // positive values for right side
-    backgroundColor: 'pink'
-  }, {
-    label: 'Other',
-    data: [-180], // positive values for right side
-    backgroundColor: 'red'
-  }]
+const colors = {
+  "White British": "blue",
+  "White Other": "green",
+  "Other": "grey",
+  "Asian": "brown",
+  "Black": "black",
+  "Mixed": "purple",
 };
 
-const config = {
-  type: 'bar',
-  data: data,
+const pyramidData = {
+  labels: Object.keys(rawData[0])
+    .toReversed()
+    .filter((x) => !["Ethnicity", "Sex"].includes(x)),
+  datasets: [],
+};
+
+const chartData = structuredClone(pyramidData);
+const percentagesData = structuredClone(pyramidData);
+
+for (const group of rawData.toReversed()) {
+  console.log(group);
+  // pyramidData.labels.push(ageGroup.Age);
+  if (group.Sex === 'All') {
+    if (group.Ethnicity === 'All') {
+      continue;
+    }
+    const rawValues = [];
+    for (const val of Object.values(group)) {
+      if (!isNaN(val)) {
+        rawValues.push(val);
+      }
+    }
+    chartData.datasets.unshift({
+      label: group.Ethnicity,
+      data: rawValues,
+      backgroundColor: colors[group.Ethnicity],
+    });
+  } else {
+    const label = `${group.Ethnicity} ${group.Sex}`;
+    const coefficient = group.Sex === "Male" ? -1 : 1;
+    const rawValues = [];
+    for (const val of Object.values(group).toReversed()) {
+      if (!isNaN(val)) {
+        rawValues.push(val * coefficient);
+      }
+    }
+    pyramidData.datasets.unshift({
+      label,
+      data: rawValues,
+      backgroundColor: colors[group.Ethnicity],
+    });
+  }
+}
+
+// console.log(pyramidData);
+
+
+
+const pyramidConfig = {
+  type: "bar",
+  data: pyramidData,
   options: {
-    indexAxis: 'y',
+    indexAxis: "y",
     scales: {
       x: {
         stacked: true,
         ticks: {
-          callback: (value) => Math.abs(value) // this makes the labels positive
-        }
+          callback: (value) => Math.abs(value), // this makes the labels positive
+        },
       },
       y: {
-        stacked: true
-      }
-    }
-  }
+        stacked: true,
+      },
+    },
+  },
 };
 
-new Chart(ctx, config);
+const chartConfig = {
+  type: "bar",
+  data: chartData,
+  options: {
+    // indexAxis: "y",
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          callback: (value) => Math.abs(value), // this makes the labels positive
+        },
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  },
+};
+
+const percentagesConfig = {
+  type: "bar",
+  data: percentagesData,
+  options: {
+    // indexAxis: "y",
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          callback: (value) => Math.abs(value), // this makes the labels positive
+        },
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  },
+};
+
+new Chart(ctx1, pyramidConfig);
+new Chart(ctx2, chartConfig);
+new Chart(ctx3, percentagesConfig);
